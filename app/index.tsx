e4ea122cc200fce840a8876202476393706b4f10
CollinsEzeokeke <collinsmecosy@gmail.com>
2024-11-21 17:45:10 -0800

    completed my configurations for this project

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text className="">Hello world my name is collins and this is my first web app</Text>
+    </View>
+  );
+}
